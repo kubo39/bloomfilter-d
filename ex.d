899ab65120a0967620cc1fc24b83c22b0f761c53
Hiroki Noda <kubo39@gmail.com>
2013-03-07 13:13:41 +0900

    add more example for bloomfilter

--- ex.d
+++ ex.d
@@ -7,11 +7,21 @@ void main() {
     auto bf = new Bloomfilter(20*n, 5);
 
     auto l = cast(ubyte[]) "Love";
+    // Add "Love" to bloomfilter
     bf.Add(l);
+
+    // Test "Love"
     auto flag = bf.Test(l);
-    writeln(flag);
+    writeln(flag); // should be true
 
     auto sd = cast(ubyte[]) "^^aacsv";
-    auto flag2 = bf.Test(sd);
+    auto flag2 = bf.Test(sd);     // should be false
     writeln(flag2);
+
+    // Add "^^aacsv" to bloomfilter
+    bf.Add(sd);
+    writeln(bf.Test(l)); // should be true
+
+    bf.clearAll();
+    writeln(bf.Test(l)); // should be false
 }
